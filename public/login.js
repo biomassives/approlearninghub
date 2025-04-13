d46e7c15fff5fe285314216a45c1f86107277d85
G. Willson <greg@ecocity.com>
2025-04-12 22:53:51 -0600

    /api/auth routing

--- public/login.js
+++ public/login.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async (e) => {
   errorContainer.classList.add('hidden');
 
   try {
-    const res = await fetch('/api/login', {
+    const res = await fetch('/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
